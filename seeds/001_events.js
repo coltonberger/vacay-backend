de2391a3c4ac2f378132665067a86c55bd60a680
Colton Berger <coltonberger@optonline.net>
2018-08-07 16:44:33 -0700

    updated pics

--- seeds/001_events.js
+++ seeds/001_events.js
@@ -8,7 +8,7 @@ exports.seed = function (knex, Promise) {
         {
           id: 1,
           eventName: 'Explore Alcatraz',
-          eventImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alcatraz_Island_photo_D_Ramey_Logan.jpg/1200px-Alcatraz_Island_photo_D_Ramey_Logan.jpg',
+          eventImage: 'https://www.nationalparks.org/sites/default/files/Alcatraz_iStock_000012379929.jpg',
           eventWebsite: 'https://www.alcatrazcruises.com/',
           eventDescription: 'Experience the beauty, history and infamy of Alcatraz Island on San Francisco Bay. You will be astounded by what you see and learn.',
           eventPrice: 37,
@@ -16,7 +16,7 @@ exports.seed = function (knex, Promise) {
         }, {
           id: 2,
           eventName: 'Bike Golden Gate Bridge',
-          eventImage: 'http://www.argonauthotel.com/~/media/argonaut/activities/270/bike-bridge%20jpg.ashx',
+          eventImage: 'https://baycitybike.com/wp-content/uploads/SOSF-FERRY-BUILDING-1.jpg',
           eventWebsite: 'https://www.blazingsaddles.com/san-francisco',
           eventDescription: 'Bike the Bridge! Our most popular route takes you along San Francisco’s stunning waterfront, across the Golden Gate Bridge and down to the picturesque seaside towns of Sausalito & Tiburon. Return via ferry!',
           eventPrice: 45,
@@ -40,7 +40,7 @@ exports.seed = function (knex, Promise) {
         }, {
           id: 5,
           eventName: 'Empire State Building',
-          eventImage: 'http://www.esbnyc.com/sites/default/files/styles/module_image__600x338_/public/popular.jpg?itok=Y6rysx3v',
+          eventImage: 'https://wingsair.net/wp-content/uploads/2015/05/Manhattan-sunset.jpg',
           eventWebsite: 'http://www.esbnyc.com/',
           eventDescription: 'Iconic, art deco office tower from 1931 with exhibits & observatories on the 86th & 102nd floors.',
           eventPrice: 30,
@@ -48,7 +48,7 @@ exports.seed = function (knex, Promise) {
         }, {
           id: 6,
           eventName: 'Museum of Modern Art',
-          eventImage: 'https://assets.harlemspirituals.com/fourd/hsn-img4D/moma-museum-of-modern-art-andy-warhol-900x435.jpg',
+          eventImage: 'http://assets.saatchiart.com/saatchi/1056993/art/4244559/3314407-KESXFDWW-8.jpg',
           eventWebsite: 'https://www.moma.org/tickets/select',
           eventDescription: 'Works from van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The Modern restaurant.',
           eventPrice: 45,
@@ -56,7 +56,7 @@ exports.seed = function (knex, Promise) {
         }, {
           id: 7,
           eventName: 'Hike to the Hollywood Sign',
-          eventImage: 'https://i2.wp.com/wearetravelgirls.com/wp-content/uploads/2016/08/photo-1460881680858-30d872d5b530.jpeg?resize=1080%2C719&ssl=1',
+          eventImage: 'https://i.pinimg.com/originals/fa/cd/30/facd3096ae5f9527e5ac228644e90607.jpg',
           eventWebsite: 'https://hollywoodsign.org/',
           eventDescription: "It's more than just nine white letters spelling out a city's name; it's one of the world's most evocative symbols – a universal metaphor for ambition, success, glamour...for this dazzling place, industry and dream we call H-O-L-L-Y-W-O-O-D",
           eventPrice: 0,
@@ -64,7 +64,7 @@ exports.seed = function (knex, Promise) {
         }, {
           id: 8,
           eventName: 'Griffith Observatory',
-          eventImage: 'https://media.timeout.com/images/104702700/380/285/image.jpg',
+          eventImage: 'https://oligibbs.com/photos/images/2015-10-29-1446097500.jpg',
           eventWebsite: 'http://griffithobservatory.org/visiting/admission.html',
           eventDescription: 'The vista from this hilltop landmark is stunning, particularly at night when Los Angeles twinkles below. Inside you’ll find a bevy of exhibits, including a Foucault pendulum, Tesla coil and planetarium show',
           eventPrice: 30,
@@ -72,7 +72,7 @@ exports.seed = function (knex, Promise) {
         }, {
           id: 9,
           eventName: 'Santa Monica Pier',
-          eventImage: 'https://media.timeout.com/images/103978390/380/285/image.jpg',
+          eventImage: 'http://assets.saatchiart.com/saatchi/675832/art/2040379/1152786-KIKVTKMS-8.jpg',
           eventWebsite: 'https://santamonicapier.org/',
           eventDescription: 'Considered the focal point of Santa Monica Beach, Santa Monica Pier includes a Ferris wheel, aquarium, fairground games and cotton candy stands.',
           eventPrice: 0,
