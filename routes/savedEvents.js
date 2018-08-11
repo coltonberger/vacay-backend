f9c5ae7d8eca34ff3a6e9c4af991725e1f4be3bc
Colton Berger <coltonberger@optonline.net>
2018-08-10 17:46:26 -0700

    created join table to pull event info

--- routes/savedEvents.js
+++ routes/savedEvents.js
@@ -4,49 +4,24 @@ const knex = require('../db/knex')
 
 /* GET savedEvent listing. */
 router.get('/', function (req, res, next) {
-  knex('savedEvents')
-    .then((savedEvents) => {
-      let newSavedEventsArr = savedEvents.map((savedEvent) => {
-        return savedEvent
-      })
-      res.status(200).send(newSavedEventsArr) // 200 = ok
-    })
-    .catch((err) => {
-      console.log('err', err)
-      res.status(500).send({error: {message: 'Something went wrong!'}})
-    })
+  knex
+  .select('events.id','events.eventName')
+  .from('events')
+  .innerJoin('savedEvents', 'events_id','events.id')
+  .where('events.id' !== null)
+  .then(data => res.json(data));
+
+
+  // knex('savedEvents')
+  //   .then((savedEvents) => {
+  //     res.status(200).send(savedEvents) // 200 = ok
+  //   })
+  //   .catch((err) => {
+  //     console.log('err', err)
+  //     res.status(500).send({error: {message: 'Something went wrong!'}})
+  //   })
 })
 
-// get ONE schedule
-router.get('/:savedEventsid', (req, res, next) => {
-  knex('savedEvents')
-    .where('id', req.params.savedEventsid)
-    .then((savedEvents) => {
-      let newSavedEventsArr = savedEvents.map((savedEvent) => {
-      // console.log('schedule is', schedule)
-        return savedEvent
-      })
-      console.log('the specific schedule', newSavedEventsArr)
-      res.send(newSavedEventsArr)
-    })
-})
-
-//create one schedule <<<OK>>>
-router.post('/', (req, res, next) => {
-  console.log('REQ.BODY', req.body);
-  knex('schedules')
-  .insert(
-    {userId: req.body.schedules_id},
-    {eventId: req.body.events_id}
-  )
-  .returning('*')
-  .then((result) => {
-    console.log(result)
-    let insertedRecord = result[0]
-    console.log('data', insertedRecord)
-    res.send(insertedRecord)
-  })
-})
 
 
 // DELETE a specific schedule <<<OK>>>
@@ -69,29 +44,29 @@ router.post('/', (req, res, next) => {
 // })
 
 //DELETE one schedule
-router.delete('/:id', function(req, res, next) {
-  const scheduleId = req.params.id;
-  console.log(scheduleId)
-
-  knex('schedules')
-    .where('id', scheduleId)
-    .then((row) => {
-      if(!row) return next()
-      knex('schedules')
-        .del()
-        .where('id', scheduleId)
-        .then(() => {
-          res.send(`ID ${scheduleId} Deleted`)
-        })
-        .catch((err) => {
-          console.log("Hey, could not delete!")
-          next(err)
-        })
-    })
-    .catch((err) => {
-      next(err)
-    })
-})
+// router.delete('/:id', function(req, res, next) {
+//   const scheduleId = req.params.id;
+//   console.log(scheduleId)
+//
+//   knex('schedules')
+//     .where('id', scheduleId)
+//     .then((row) => {
+//       if(!row) return next()
+//       knex('schedules')
+//         .del()
+//         .where('id', scheduleId)
+//         .then(() => {
+//           res.send(`ID ${scheduleId} Deleted`)
+//         })
+//         .catch((err) => {
+//           console.log("Hey, could not delete!")
+//           next(err)
+//         })
+//     })
+//     .catch((err) => {
+//       next(err)
+//     })
+// })
 
 
 
